74a1f70473f3b6edd0feec73c43321f4740f9cc6
tony <fengfeng043@gmail.com>
2019-06-30 23:09:15 +0800

    读写都要加锁

---
+++ src/c_008/Account.java
@@ -0,0 +1,54 @@
+package c_008;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 对业务写方法加锁，而对业务读方法不加锁，
+ *
+ * 容易出现 脏读问题
+ * <p>
+ * 因为，在执行写的过程中，因为读操作没有加锁，所以读会读取到写未改完的脏数据
+ * 解决办法，给读写都加锁
+ */
+public class Account {
+
+    /**
+     * 银行账户名称
+     */
+    String name;
+    /**
+     * 银行账余额
+     */
+    double balance;
+
+    public synchronized void set(String name, double balance) {
+        this.name = name;
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        this.balance = balance;
+    }
+
+    /**
+     * 要加锁的
+     * @return
+     */
+    public /*synchronized*/ double getBalance() {
+        return this.balance;
+    }
+
+    public static void main(String[] args) {
+        Account account = new Account();
+        new Thread(() -> account.set("张三", 100.0)).start();
+        System.out.println(account.getBalance()); // 0.0
+
+//        try {
+//            TimeUnit.SECONDS.sleep(3);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        System.out.println(account.getBalance()); // 应该是100.0
+    }
+}
