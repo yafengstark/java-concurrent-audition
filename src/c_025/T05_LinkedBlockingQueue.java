cf3f50d98f05f265c866808442c6303569d3c79e
tony <fengfeng043@gmail.com>
2019-07-08 17:03:03 +0800

    LinkedBlockingQueue 完成生产者消费者模式

--- src/c_025/T05_LinkedBlockingQueue.java
+++ src/c_025/T05_LinkedBlockingQueue.java
@@ -9,6 +9,8 @@ import java.util.concurrent.TimeUnit;
 /**
  * 使用阻塞同步队列 LinkedBlockingQueue 完成生产者消费者模式
  * 使用场景较多。
+ *
+ * 
  */
 public class T05_LinkedBlockingQueue {
 
@@ -21,7 +23,8 @@ public class T05_LinkedBlockingQueue {
         new Thread(() -> {
             for (int j = 0; j < 100; j++) {
                 try {
-                    queue.put("aaa" + j); // put 方法，给容器添加元素，如果容器已经满了，则会阻塞等待
+                    queue.put("aaa" + j);
+                    // put 方法，给容器添加元素，如果容器已经满了，则会阻塞等待
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -39,7 +42,8 @@ public class T05_LinkedBlockingQueue {
             new Thread(() -> {
                 while (true) {
                     try {
-                        System.out.println(Thread.currentThread().getName() + ":" + queue.take()); // 从队列中拿数据，如果空了，则会阻塞等待
+                        System.out.println(Thread.currentThread().getName() + ":" + queue.take());
+                        // 从队列中拿数据，如果空了，则会阻塞等待
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
